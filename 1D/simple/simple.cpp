16a457ad711c0958ffe3b71dda8e0a6e7e998df7
GSoaresgama <gabrielsgamma@gmail.com>
2020-04-25 23:18:01 -0300

    excel

--- 1D/simple/simple.cpp
+++ 1D/simple/simple.cpp
@@ -9,7 +9,7 @@
 #define LENGHT 1800
 #define MAX_Y 6.0
 #define X_LIMITS 75
-#define INDV_COUNT 50
+#define INDV_COUNT 100
 #define START_MUT 50
 #define MUT_DECREASE_RATE 0.80
 #define MUT_INCREASE_RATE 2
@@ -249,8 +249,8 @@ int main()
 
             if (best > MAX_VALUE - ERROR)
             {
-                cout << best << "  ";
-                cout << gen << "  ";
+                //cout << best << " , ";
+                cout << gen << " , ";
                 cout << test << endl;
 
                 break;
